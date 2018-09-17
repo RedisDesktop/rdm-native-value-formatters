91f15077616b28291b31bb80315142425a8ec0ec
ommrnom <ommarinaom@gmail.com>
2018-09-17 17:45:30 +0300

    Wait for stdin value instead of base64 encoded argument

--- python-decompresser/formatter.py
+++ python-decompresser/formatter.py
@@ -1,6 +1,3 @@
-import argparse
-import base64
-import binascii
 import json
 import gzip
 import lz4
@@ -8,18 +5,20 @@ import lzma
 import snappy
 import sys
 
+from python_utils.helpers import get_arg_parser, wait_for_stdin_value
+
 __version__ = "0.0.1"
+DESCRIPTION = "python native decompressing formatter with gzip, lzma, lz4 " \
+              "and snappy support"
 
 ACTION_VALIDATE = "validate"
 ACTION_DECODE = "decode"
 
 actions = (ACTION_DECODE, ACTION_VALIDATE)
 
-parser = argparse.ArgumentParser(
-    description='python native decompressing formatter with gzip, lzma, lz4 and snappy support %s' % __version__)
-parser.add_argument('-v', '--version', action='version', version=__version__)
-parser.add_argument('action', help="Available actions: %s" % str(actions))
-parser.add_argument('value', help="Value encoded with base64")
+parser = get_arg_parser(description=DESCRIPTION,
+                        version=__version__,
+                        actions=actions)
 
 
 def is_gzip(value):
@@ -28,7 +27,8 @@ def is_gzip(value):
 
 def is_lzma(value):
     return len(value) >= 26 and value[:26] == \
-                                b'\xfd7zXZ\x00\x00\x04\xe6\xd6\xb4F\x02\x00!\x01\x16\x00\x00\x00t/\xe5\xa3\x01\x00'
+                                b'\xfd7zXZ\x00\x00\x04\xe6\xd6\xb4F\x02\x00!' \
+                                b'\x01\x16\x00\x00\x00t/\xe5\xa3\x01\x00'
 
 
 def is_snappy(value):
@@ -52,20 +52,19 @@ def main():
             print(msg)
             sys.exit(2)
 
-    try:
-        decoded_value = base64.b64decode(args.value)
-    except binascii.Error as e:
-        return process_error("Cannot decode value: %s" % e)
+    value = wait_for_stdin_value()
+    if not value:
+        return process_error("No value to format.")
 
     try:
-        if is_gzip(decoded_value):
-            unpacked_value = gzip.decompress(decoded_value)
-        elif is_lzma(decoded_value):
-            unpacked_value = lzma.decompress(decoded_value)
-        elif is_snappy(decoded_value):
-            unpacked_value = snappy.uncompress(decoded_value)
+        if is_gzip(value):
+            unpacked_value = gzip.decompress(value)
+        elif is_lzma(value):
+            unpacked_value = lzma.decompress(value)
+        elif is_snappy(value):
+            unpacked_value = snappy.uncompress(value)
         else:
-            unpacked_value = lz4.block.decompress(decoded_value)
+            unpacked_value = lz4.block.decompress(value)
     except OSError as e:
         return process_error("Cannot decompress value: %s" % e)
 
--- python-msgpack/formatter.py
+++ python-msgpack/formatter.py
@@ -1,21 +1,20 @@
-import argparse
-import base64
-import binascii
 import json
 import msgpack
 import sys
 
+from python_utils.helpers import get_arg_parser, wait_for_stdin_value
+
 __version__ = "0.0.1"
+DESCRIPTION = "python msgpack native formatter"
 
 ACTION_VALIDATE = "validate"
 ACTION_DECODE = "decode"
 
 actions = (ACTION_DECODE, ACTION_VALIDATE)
 
-parser = argparse.ArgumentParser(description='python msgpack native formatter %s' % __version__)
-parser.add_argument('-v', '--version', action='version', version=__version__)
-parser.add_argument('action', help="Available actions: %s" % str(actions))
-parser.add_argument('value', help="Value encoded with base64")
+parser = get_arg_parser(description=DESCRIPTION,
+                        version=__version__,
+                        actions=actions)
 
 
 def main():
@@ -35,13 +34,12 @@ def main():
             print(msg)
             sys.exit(2)
 
-    try:
-        decoded_value = base64.b64decode(args.value)
-    except binascii.Error as e:
-        return process_error("Cannot decode value: %s" % e)
+    value = wait_for_stdin_value()
+    if not value:
+        return process_error("No value to format.")
 
     try:
-        unpacked_value = msgpack.unpackb(decoded_value, encoding='utf-8')
+        unpacked_value = msgpack.unpackb(value, encoding='utf-8')
     except msgpack.UnpackValueError as e:
         return process_error("Cannot unpack value: %s" % e)
 
@@ -56,6 +54,7 @@ def main():
             "read-only": True,
             "format": "plain_text",
         }))
+
 
 if __name__ == "__main__":
     main()
