27e9823f9201a14de06d75e419c9055bdf7fac45
Igor Malinovskiy <u.glide@gmail.com>
2019-07-04 13:08:00 +0300

    Drop unsupported and embedded formatters

--- README.md
+++ README.md
@@ -1,77 +1,15 @@
 # RedisDesktopManager Native Value Formatters
 
+Native value formatters are scripts or executables developed in different programming languages which allow to display in readable form the key values of various formats inside the RDM value editor. You can easily implement any formatter for your own purposes as well.
+
 ![91f4b202-89a4-11e5-8446-b34f21ee9152](https://cloud.githubusercontent.com/assets/1655867/20011127/315cb0c4-a2b3-11e6-8479-ae8a6d030f40.png)
 
-**Advantages:**
-- Plugin can be developed by any software engineer (Java/C++/C#/Python/PHP/Node.js etc)
-- Plugin can be easily debugged
+### [API Docs](http://docs.redisdesktop.com/en/latest/native-formatters/)
 
-## How to implement native value formatter
-1. Create directory with meaningful name. Name of the directory will be used as formatter name.
-2. Implement script or executable which meets protocol described below. 
-3. Add file `usage.json` with usage shell command<br />
-For example for python script it will be: `['python', 'my_super_formatter.py']`
-4. Test your formatter in RDM!
+### Examples
 
-## Protocol
-### Formatter info:
+You can find example of native formatter in `python-decompresser` folder
 
-CMD: `<executable> info`
-STDOUT (json):
+#### Feel free to submit PR with your native formatter to this repo.
 
-```
-{
-    "version": "1.0.0",
-    "description": "Formatter description"
-}
-```
-### Validate: 
-
-CMD: `<executable> validate`
-STDIN: `<binary-data-encoded-with-base64>`
-STDOUT (json): 
-
-```
-{    
-    “valid”: “false”,    
-    “error”: “input is not gziped data” // if valid is false
-}
-```
-
-
-### Decode binary data:  
-
-CMD: `<executable> decode`
-STDIN: `<binary-data-encoded-with-base64>`
-STDOUT (json): 
-
-```
-{
-    “output”: “<decoded data for human-friendly edit>”,
-    “read-only”: “false”,    
-    “format”: “plain_text” // or “json”
-}
-```
-### Encode string-representation 
-**Required only for formatters which return `“read-only”: false` in `decode` method**
-
-CMD: `<executable> encode`
-STDIN : `<edited-string-representation-encoded-with-base64>`
-STDOUT (json): 
-```
-{
-    “output”: “<binary-data-encoded-with-base64>”,    
-}
-
-```
-
-### Error handling:
-If formatter cannot decode/encode value error response should be returned:
-
-OUT (json): 
-```
-{
-    “error”: “Invalid CBOR data”,    
-}
-```
 
--- python-decompresser/formatter.py
+++ python-decompresser/formatter.py
@@ -1,3 +1,10 @@
+from abc import ABC, abstractmethod
+import binascii
+import json
+import logging
+import argparse
+import base64
+import time
 import os
 import sys
 
@@ -13,25 +20,142 @@ except ImportError:
 sys.path.append(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from python_utils.base import BaseFormatter
 
 __version__ = '0.0.1'
 DESCRIPTION = 'Python native decompressing formatter with gzip, lzma, lz4 ' \
               'and snappy support'
 
 
-def is_gzip(value):
-    return len(value) >= 3 and value[:3] == b'\x1f\x8b\x08'
+TIMEOUT = 5
 
 
-def is_lzma(value):
-    return len(value) >= 26 and value[:26] == \
-                                b'\xfd7zXZ\x00\x00\x04\xe6\xd6\xb4F\x02\x00!' \
-                                b'\x01\x16\x00\x00\x00t/\xe5\xa3\x01\x00'
+def get_arg_parser(description, version, actions):
+    parser = argparse.ArgumentParser(
+        description='{} {}'.format(description, version))
+    parser.add_argument('action', choices=actions,
+                        help='Available actions: {}'.format(actions))
+    return parser
 
 
-def is_snappy(value):
-    return snappy.isValidCompressed(value)
+def wait_for_stdin_value(timeout=TIMEOUT):
+    stop = time.time() + timeout
+    while time.time() < stop:
+        try:
+            value = sys.stdin.read()
+            return base64.b64decode(value)
+        except Exception:
+            time.sleep(0.1)
+    return None
+
+
+class BaseFormatter(ABC):
+    ACTION_DECODE = 'decode'
+    ACTION_INFO = 'info'
+    ACTION_VALIDATE = 'validate'
+
+    actions = (ACTION_DECODE, ACTION_INFO, ACTION_VALIDATE)
+
+    def __init__(self, debug=True):
+        self.logger = logging.getLogger()
+
+        if debug:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.INFO)
+
+    @property
+    @abstractmethod
+    def description(self):
+        return DESCRIPTION
+
+    @property
+    @abstractmethod
+    def version(self):
+        return __version__
+
+    @abstractmethod
+    def format(self, value):
+        raise NotImplementedError()
+
+    def process_error(self, message):
+        if self.action == self.ACTION_VALIDATE:
+            print(json.dumps({
+                'valid': False,
+                'error': message
+            }))
+        else:
+            self.logger.error(message)
+            sys.exit(2)
+
+    def validate_action(self, action):
+        if action not in self.actions:
+            self.logger.error('Error: Invalid action {}'.format(action))
+            sys.exit(1)
+        self.action = action
+
+    @staticmethod
+    def valid_output():
+        print(json.dumps({
+            'valid': True,
+            'error': ''
+        }))
+
+    def info_output(self):
+        print(json.dumps({
+            'version': self.version,
+            'description': self.description
+        }))
+
+    @staticmethod
+    def formatted_output(output):
+        def get_output_dict(output):
+            return {
+                'output': output,
+                'read-only': True,
+                'format': 'plain_text',
+            }
+
+        if hasattr(output, 'decode'):
+            output = output.decode()
+
+        try:
+            json_output = json.dumps(get_output_dict(output))
+        except (TypeError, OverflowError):
+            json_output = json.dumps(get_output_dict(repr(output)))
+
+        print(json_output)
+
+    def main(self, *args):
+        parser = get_arg_parser(description=self.description,
+                                version=self.version,
+                                actions=self.actions)
+        if args:
+            args = parser.parse_args(args)
+        else:
+            args = parser.parse_args()
+
+        self.validate_action(args.action)
+
+        if self.action == self.ACTION_INFO:
+            return self.info_output()
+
+        try:
+            value = wait_for_stdin_value()
+        except binascii.Error as e:
+            return self.process_error('Cannot decode value: {}'.format(e))
+
+        if not value:
+            return self.process_error('No value to format.')
+
+        try:
+            output = self.format(value=value)
+        except Exception as e:
+            return self.process_error('Cannot format value: {}'.format(e))
+
+        if self.action == self.ACTION_VALIDATE:
+            return self.valid_output()
+
+        return self.formatted_output(output)
 
 
 class DecompressingFormatter(BaseFormatter):
@@ -39,6 +163,18 @@ class DecompressingFormatter(BaseFormatter):
     version = __version__
 
     def format(self, value):
+
+        def is_gzip(value):
+            return len(value) >= 3 and value[:3] == b'\x1f\x8b\x08'
+
+        def is_lzma(value):
+            return len(value) >= 26 and value[:26] == \
+                   b'\xfd7zXZ\x00\x00\x04\xe6\xd6\xb4F\x02\x00!' \
+                   b'\x01\x16\x00\x00\x00t/\xe5\xa3\x01\x00'
+
+        def is_snappy(value):
+            return snappy.isValidCompressed(value)
+
         try:
             if is_gzip(value):
                 output = gzip.decompress(value)
